6352f3752e392595d08b4ce9f50a82151a016f3d
Isaque Veloso <isaqueveloso29@gmail.com>
2026-09-01 12:38:10 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,23 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+// Carrega um usuário
+// $root = new Usuario();
+// $root->loadById(4);
+// echo $root;
 
-$root->loadById(4);
+// Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo $root;
+// Carrega uma lista de usuários buscando pelo login
+// $search = Usuario::search("Yasmin");
+// echo json_encode($search);
+
+// Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("Yasmin", "545464576");
+
+echo $usuario;
 
 ?>
